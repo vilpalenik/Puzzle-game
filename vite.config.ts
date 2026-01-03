980c7602806106fa3a9e597e20f1ebaf9089cc89
Viliam Páleník <vil.palenik@gmail.com>
2026-01-03 15:22:41 +0100

    oprava pwa na server

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
         theme_color: '#ffffff',
         background_color: '#ffffff',
         display: 'standalone',
-        start_url: '/',
+        start_url: '/~xpalenikv/semestralne_zadanie_palenik_balla/',
         icons: [
           {
             src: 'pwa-192x192.png',
